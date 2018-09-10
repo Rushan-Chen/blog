cdc94c13c921fe4d038ecb4b426cf496198fe8d8
Rushan-Chen <szugycatherine@163.com>
2018-09-10 10:34:55 +0800

    modify posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET posts page. */
-router.get('/posts', function(req, res, next) {
-  res.render('posts', { title: '无影有记' });
+router.get('/', function(req, res, next) {
+  res.render('posts', { title: '文章' });
 });
 
 module.exports = router;
