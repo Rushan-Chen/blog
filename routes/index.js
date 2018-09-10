4cb7b714fd6623209c62bd997b7f48a72ed3f71e
Rushan-Chen <szugycatherine@163.com>
2018-09-10 17:04:32 +0800

    edit ejs template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+// 如果手动引入ejs:
+// var ejs = require('ejs');
+// var path = require('path');
+// var fs = require('fs');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: '无影有记' });
+
+  // 如果手动引入ejs:
+  // var pathString = path.join(__dirname, '../views/index.ejs');
+  // var content = fs.readFileSync(pathString, 'utf-8');
+  // var html = ejs.render(content, {title: 'hello world'});
+  // res.send(html);
 });
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET posts page. */
 router.get('/', function(req, res, next) {
-  res.render('posts', { title: '文章' });
+  res.render('posts', { title: '文章', postsList: ['文章1', '文章2', '文章3'] });
 });
 
 module.exports = router;
