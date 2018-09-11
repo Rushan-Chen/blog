6a163c80b3cd07b8e27cf0aab72a0a39d0be6b32
Rushan-Chen <szugycatherine@163.com>
2018-09-11 08:21:20 +0800

    render html in client

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET posts page. */
 router.get('/', function(req, res, next) {
-  res.render('posts', { title: '文章', postsList: ['文章1', '文章2', '文章3'] });
+  res.render('posts', { title: '文章' });
+});
+
+/* GET 获取postsList数据 */
+router.get('/list', function(req, res, next) {
+  res.json({ postsList: ['文章1', '文章2', '文章3'] });
 });
 
 module.exports = router;
