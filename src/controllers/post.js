eaf96b3a0c9361fe7cc020297e52bbe1f4c2fcbc
Rushan-Chen <szugycatherine@163.com>
2018-09-26 16:06:47 +0800

    promise

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,25 +1,23 @@
 import PostModel from '../models/post' ;
 
 export const more = function(req, res, next) {
-    PostModel.find ({}, {}, function (err, posts){
-        if (err) {
-            next(err);
-        } else {
+    PostModel.find ({}, {})
+        .exec()
+        .then( posts => {
             res.json({ postsList: posts });
-        }
-    });
+        })
+        .catch(next);
 };
 
 export const one = function(req, res, next) {
     const id = req.params.id;
 
-    PostModel.findById ( id, function (err, post){
-        if (err) {
-            next(err);
-        } else {
+    PostModel.findById ( id )
+        .exec()
+        .then(post => {
             res.json({ post });
-        }
-    });
+        })
+        .catch(next);
 };
 
 export const create = function(req, res, next) {
@@ -30,24 +28,21 @@ export const create = function(req, res, next) {
     post.content = content;
     post.authorId = res.locals.currentUser._id;
     
-    post.save(function (err, doc) {
-        if(err) {
-            next(err);
-        } else {
-            res.json({ post: doc }); // 创建后返回数据给客户端，客户端拿到id可以做一些交互
-        }
-    });
+    post.save()
+        .then(doc => {
+            res.json({ post: doc });// 创建后返回数据给客户端，客户端拿到id可以做一些交互
+        })
+        .catch(next);
 };
 
 export const update = function(req, res, next) {
     const id = req.params.id;
     const { title, content } = req.body;
 
-    PostModel.findOneAndUpdate({ _id: id }, { title, content }, function(err) {
-        if (err) {
-            next(err);
-        } else {
+    PostModel.findOneAndUpdate({ _id: id }, { title, content })
+        .exec()
+        .then(() => {
             res.json({}); // 不需要返回文章数据
-        }
-    });
+        })
+        .catch(next);
 };
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,14 +1,14 @@
 import bcrypt from 'bcrypt';
-import UserModel from '../models/user' ;
-import config from '../config' ;
+import UserModel from '../models/user';
+import config from '../config';
 import jwt from 'jwt-simple';
 import moment from 'moment';
 import { sendActiveMail } from '../common/mail';
 import utility from 'utility';
 
-export const signup = function(req, res, next) {
+export const signup = function (req, res, next) {
     const { name, email, pass, rePass } = req.body;
-    
+
     if (!name || !email || !pass || !rePass) {
         return next(new Error('不能为空'));
     }
@@ -22,10 +22,8 @@ export const signup = function(req, res, next) {
     user.name = name;
     user.email = email;
     user.pass = bcrypt.hashSync(pass, 10);
-    user.save(function (err) {
-        if(err) {
-            next(err);
-        } else {
+    user.save()
+        .then(() => {
             sendActiveMail(
                 email,
                 utility.md5(user.email + user.pass),
@@ -33,21 +31,20 @@ export const signup = function(req, res, next) {
             );
 
             res.json({
-                message:`欢迎加入${
+                message: `欢迎加入${
                     config.name
-                }! 已经发了一封邮件到你的注册邮箱，请点击里面的激活链接来激活你的账户。` 
-            }); 
-        }
-    });
+                    }! 已经发了一封邮件到你的注册邮箱，请点击里面的激活链接来激活你的账户。`
+            });
+        })
+        .catch(next);
 };
 
-export const signin = function(req, res, next) {
+export const signin = function (req, res, next) {
     const { name, pass } = req.body;
-    
-    UserModel.findOne ({ name }, function (err, user){
-        if (err || !user) {
-            return next(new Error('找不到用户'));
-        } else {
+
+    UserModel.findOne({ name })
+        .exec()
+        .then(user => {
             const isOk = bcrypt.compareSync(pass, user.pass);
             if (!isOk) {
                 return next(new Error('密码不正确'));
@@ -55,76 +52,74 @@ export const signin = function(req, res, next) {
             if (!user.active) {
                 return next(new Error('需要激活'));
             }
-        }
 
-        const token = jwt.encode(
-            {
-                _id: user._id,
-                name: user.name,
-                isAdmin: user.name === config.admin ? true : false,
-                exp: moment().add( 30, 'days' ).valueOf() 
-            },
-            config.jwtSecret
-        );
-        const opts = {
-            path: '/',
-            maxAge: moment().add( 30, 'days' ).valueOf(), //【重要】cookie有效期30天。单位是毫秒。
-            signed: true, // 签名
-            httpOnly: true // 将 cookie 标记为只能由 web 服务器访问。
-        };
+            const token = jwt.encode(
+                {
+                    _id: user._id,
+                    name: user.name,
+                    isAdmin: user.name === config.admin ? true : false,
+                    exp: moment().add(30, 'days').valueOf()
+                },
+                config.jwtSecret
+            );
+            const opts = {
+                path: '/',
+                maxAge: moment().add(30, 'days').valueOf(), //【重要】cookie有效期30天。单位是毫秒。
+                signed: true, // 签名
+                httpOnly: true // 将 cookie 标记为只能由 web 服务器访问。
+            };
 
-        // 将token保存在cookie里。
-        res.cookie(config.cookieName, token, opts);
-        res.json({ token });
-    });
+            // 将token保存在cookie里。
+            res.cookie(config.cookieName, token, opts);
+            res.json({ token });
+        })
+        .catch(err => next(new Error('找不到用户')));
 };
 
-export const more = function(req, res, next) {
+export const more = function (req, res, next) {
     res.send('response with a resource');
 };
 
 export const activeAccount = function (req, res, next) {
     const { key, name } = req.query;
-  
-    UserModel.findOne({ name }, function (err, user) {
-      if (err || !user) {
-        return next(new Error('找不到用户'));
-      } else {
-  
-        const key2 = utility.md5(user.email + user.pass);
-        if (key !== key2) {
-          return next(new Error('激活失败'));
-        }
-  
-        user.active = true;
-        user.save();
-  
-        const token = jwt.encode(
-          {
-            _id: user._id,
-            name: user.name,
-            isAdmin: user.name === config.admin,
-            active: user.active,
-            exp: moment()
-              .add(30, 'days')
-              .valueOf()
-          },
-          config.jwtSecret
-        );
-  
-        const opts = {
-          path: '/',
-          maxAge: moment()
-            .add(30, 'days')
-            .valueOf(),
-          signed: true,
-          httpOnly: true
-        };
-  
-        res.cookie(config.cookieName, token, opts);
-        res.send(`<br>激活成功，点击<a href="/signin">这里登录</a>`);
-      }
-    });
+
+    UserModel.findOne({ name })
+        .exec()
+        .then(user => {
+            const key2 = utility.md5(user.email + user.pass);
+            if (key !== key2) {
+                return next(new Error('激活失败'));
+            }
+
+            user.active = true;
+            user.save();
+
+            const token = jwt.encode(
+                {
+                    _id: user._id,
+                    name: user.name,
+                    isAdmin: user.name === config.admin,
+                    active: user.active,
+                    exp: moment()
+                        .add(30, 'days')
+                        .valueOf()
+                },
+                config.jwtSecret
+            );
+
+            const opts = {
+                path: '/',
+                maxAge: moment()
+                    .add(30, 'days')
+                    .valueOf(),
+                signed: true,
+                httpOnly: true
+            };
+
+            res.cookie(config.cookieName, token, opts);
+            res.send(`<br>激活成功，点击<a href="/signin">这里登录</a>`);
+        })
+        .catch(err => next(new Error('找不到用户')));
   };
 
 export const signout = function (req, res, next) {
